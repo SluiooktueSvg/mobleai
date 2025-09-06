8c2290d294bffdb1ce4256d51e69ecbde6014d03
canchilamario100 <canchilamario100@gmail.com>
2025-09-06 03:20:18 +0000

    Nuevo commit,version incompatible gradle, otra vez, 15

--- android/app/src/main/kotlin/com/selene/chat/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.selene.chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/selene/selene_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.selene.selene_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
